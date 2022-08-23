6de275cffcd98797eecdef9b3acf9d34d94e131a
Mehranlip <realmehranasadi@gmail.com>
2022-08-23 18:41:11 +0430

    add clode button in sidebar

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
+
+import { useStateContext } from './context/ContextProvider';
+
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext()
+
     return (
         <div>
             <BrowserRouter>
