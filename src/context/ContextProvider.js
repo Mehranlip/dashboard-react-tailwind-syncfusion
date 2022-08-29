e28311ad9fcf8e702f089da5d0767fadd8d89b6b
Mehranlip <realmehranasadi@gmail.com>
2022-08-29 14:25:34 +0430

    end navbar

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -14,12 +14,15 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
-
-
+    const [isClicked, setIsClicked] = useState(initialState)
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true })
+    }
+    const [screenSize, setScreenSize] = useState(undefined)
 
 
     return (
-        <StateContext.Provider value={{ activeMenu, setActiveMenu, }}>
+        <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }}>
             {children}
         </StateContext.Provider>
     );
